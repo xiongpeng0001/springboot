455d986925e86f3bf0d787a24de57368afd642b1
xiongpeng0001 <donggun1986@163.com>
2019-05-16 14:12:08 +0800

    sss

---
+++ src/main/java/com/fiberhome/microservice/SampleController.java
@@ -0,0 +1,17 @@
+package com.fiberhome.microservice;
+
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@EnableAutoConfiguration
+public class SampleController {
+
+    @RequestMapping("/")
+    @ResponseBody
+    String home() {
+        return "Hello World!";
+    }
+}
