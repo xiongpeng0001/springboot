792e265c07fb02536b4d699b373bba05ec059a8d
xiongpeng0001 <44391986+xiongpeng0001@users.noreply.github.com>
2019-06-24 17:01:53 +0800

    Update SampleController.java

--- src/main/java/com/fiberhome/microservice/SampleController.java
+++ src/main/java/com/fiberhome/microservice/SampleController.java
@@ -12,6 +12,6 @@ public class SampleController {
     @RequestMapping("/")
     @ResponseBody
     String home() {
-        return "Hello World web-hook branch01!";
+        return "Hello World web-hook master!";
     }
 }
